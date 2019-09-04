127b6167ce1c07130223f2c598a34e602d453391
Nyholm <tobias.nyholm@gmail.com>
2019-09-04 12:46:43 +0200

    Added a really slow controller

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Form\HelloType;
+use Aws\Sns\SnsClient;
+use Http\Discovery\Psr17FactoryDiscovery;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class HelloController extends AbstractController
+{
+    private $aws;
+    private $messageBus;
+    private $cache;
+
+    /**
+     *
+     * @param $aws
+     * @param $messageBus
+     */
+    public function __construct(SnsClient $aws, MessageBusInterface $messageBus, CacheInterface $cache)
+    {
+        $this->aws = $aws;
+        $this->messageBus = $messageBus;
+        $this->cache = $cache;
+    }
+
+
+    /**
+     * @Route("/", name="hello_world")
+     */
+    public function world()
+    {
+        // Crete some objects
+        $request = Psr17FactoryDiscovery::findRequestFactory()->createRequest('GET', '/foo');
+        $response = Psr17FactoryDiscovery::findResponseFactory()->createResponse(200);
+
+        // Fetch something from disk
+        $cacheKey = 'string'.uniqid();
+        $result = $this->cache->get($cacheKey, function (ItemInterface $item) {
+            $item->expiresAfter(100);
+
+            return 'foobar';
+        });
+
+        $this->cache->delete($cacheKey);
+
+        // Forms are good to have
+        $form = $this->createForm(HelloType::class);
+
+        return $this->render('helloWorld.html.twig', [
+            'form' => $form->createView(),
+            'request' => $request,
+            'response' => $response,
+        ]);
+    }
+}
---
+++ src/Form/HelloType.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class HelloType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('foo', TextType::class);
+    }
+
+}
